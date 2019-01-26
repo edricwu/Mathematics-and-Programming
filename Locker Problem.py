88be3502028e3b37f196641b31a166d3d94ace37
Edric <edricwu008@gmail.com>
2019-01-26 15:19:14 +0800

    Delete Locker Problem.py

--- Locker Problem.py
+++
@@ -1,8 +0,0 @@
-def locker(x,y):
-    locker=[False]*x
-    for i in range (1,y+1):
-        for k in range (i,x+1,i):
-            locker[k-1] = not locker[k-1]
-    return locker.count(True)
-
-print (locker(491,491))
